94aafff64d23387d85d8a22353b671d45a08a52f
mutish <mutindastacey@gmail.com>
2026-08-22 11:43:18 +0300

    feat: implement daily sync job for fetching trending media from TMDB

--- backend/src/jobs/tmdbSyncjob.js
+++ backend/src/jobs/tmdbSyncjob.js
@@ -1,1 +1,20 @@
 // TODO: Research on how to background fetching
+import cron from 'node-cron';
+import { fetchfromTMDB } from '../services/tmdbService.js';
+import Media from '../models/media.model.js';
+
+cron.schedule('0, 0, *, *, *,', async () => {
+    try {
+        console.log("Starting daily sync...");
+        const trendingData = await fetchfromTMDB('/trending/all/day');
+        for (const item of trendingData.results){
+            await Media.syncFromTMDB(item);
+        }
+
+        console.log("Sync complete and successful");
+
+    } catch (error) {
+        console.log("Error in the scheduler: ",error.message);
+    }
+});
+
